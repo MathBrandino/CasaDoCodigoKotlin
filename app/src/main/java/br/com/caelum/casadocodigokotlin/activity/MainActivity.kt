4fc05e2183287c87c46d3725572bc4a898b82292
Matheus Brandino <m.v.brandino@gmail.com>
2018-08-31 01:15:46 -0300

    removendo observer quando matar activity

--- app/src/main/java/br/com/caelum/casadocodigokotlin/activity/MainActivity.kt
+++ app/src/main/java/br/com/caelum/casadocodigokotlin/activity/MainActivity.kt
@@ -1,11 +1,11 @@
 package br.com.caelum.casadocodigokotlin.activity
 
 import android.arch.lifecycle.Observer
-import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import br.com.caelum.casadocodigokotlin.R
+import br.com.caelum.casadocodigokotlin.data.model.Livro
 import br.com.caelum.casadocodigokotlin.fragment.CarregandoFragment
 import br.com.caelum.casadocodigokotlin.fragment.ErroFragment
 import br.com.caelum.casadocodigokotlin.fragment.ListaLivrosFragment
@@ -14,6 +14,8 @@ import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private val viewModel: LivroViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,10 +23,17 @@ class MainActivity : AppCompatActivity() {
         if (savedInstanceState == null) {
             exibe(CarregandoFragment(), false)
         }
-        val viewModel : LivroViewModel by viewModel()
 
-        viewModel.lista().observe(this, Observer { exibe(ListaLivrosFragment(), false) })
-        viewModel.erro().observe(this, Observer { exibe(ErroFragment(), false) })
+        viewModel.lista().observe(this, observerLista())
+        viewModel.erro().observe(this, observerErro())
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        viewModel.lista().removeObserver(observerLista())
+        viewModel.erro().removeObserver(observerErro())
     }
 
     private fun exibe(fragment: Fragment, empilhado: Boolean) {
@@ -37,4 +46,11 @@ class MainActivity : AppCompatActivity() {
 
         transaction.commit()
     }
+
+
+    private fun observerErro(): Observer<Throwable> =
+            Observer { exibe(ErroFragment(), false) }
+
+    private fun observerLista(): Observer<List<Livro>> =
+            Observer { exibe(ListaLivrosFragment(), false) }
 }
